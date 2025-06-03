2abe3a96e477bb890bcf38b5956b3ba700ff462d
Keenan Gunter <keenangunter123@gmail.com>
2025-06-03 17:11:32 -0400

    Took out location button. Site is not served over https so it would not work once deployed. It worked locally

--- frontend/src/components/general/AppHeader.tsx
+++ frontend/src/components/general/AppHeader.tsx
@@ -2,8 +2,7 @@ import { useRef, useState } from "react";
 import CitySearchBar, {
   type CitySearchBarHandles,
 } from "./CitySearchBar";
-import MyLocationIcon from "@mui/icons-material/MyLocation";
-import { IconButton, Tooltip } from "@mui/material";
+
 import favicon from "../../assets/favicon.svg";
 
 interface AppHeaderProps {
@@ -11,30 +10,7 @@ interface AppHeaderProps {
 }
 
 const AppHeader = ({ onCityChange }: AppHeaderProps) => {
-  const [locating, setLocating] = useState(false);
   const searchBarRef = useRef<CitySearchBarHandles>(null);
-
-  const handleUseMyLocation = () => {
-    if (!navigator.geolocation) {
-      alert("Geolocation is not supported by your browser.");
-      return;
-    }
-
-    setLocating(true);
-
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        searchBarRef.current?.clear();
-        onCityChange(`${latitude},${longitude}`);
-        setLocating(false);
-      },
-      () => {
-        alert("Unable to retrieve your location.");
-        setLocating(false);
-      }
-    );
-  };
 
   return (
     <header className="sticky top-0 left-0 right-0 w-full bg-blue-800/5 backdrop-blur-md z-50">
@@ -48,16 +24,6 @@ const AppHeader = ({ onCityChange }: AppHeaderProps) => {
 
         <div className="flex items-center space-x-2">
           <CitySearchBar ref={searchBarRef} onSearch={onCityChange} />
-          <Tooltip title="Use My Location">
-            <IconButton
-              onClick={handleUseMyLocation}
-              color="primary"
-              sx={{ p: 1, "&:focus": { outline: "none" } }}
-              disabled={locating}
-            >
-              <MyLocationIcon />
-            </IconButton>
-          </Tooltip>
         </div>
       </div>
       <div className="h-[2px] w-screen bg-gradient-to-r from-black via-blue-700 shadow-md" />
