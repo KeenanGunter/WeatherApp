94643f3adaa72d24b8cdb37f4ca3e6f41ba86773
Keenan Gunter <keenangunter123@gmail.com>
2025-06-03 01:27:29 -0400

    Testing running localy and workflow both works correctly. Simple placeholder ui to see some data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,16 @@
-//import { useEffect, useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-// import AppHeader from './components/general/AppHeader';
-// import AppFooter from './components/general/AppFooter';
+import AppHeader from './components/general/AppHeader';
+import AppFooter from './components/general/AppFooter';
 import Home from './pages/Home'
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        {/* <AppHeader /> */}
+        <AppHeader />
         
         <main className="flex-grow">
           <Routes>
@@ -19,7 +18,7 @@ function App() {
           </Routes>
         </main>
         
-        {/* <AppFooter /> */}
+        <AppFooter />
       </div>
     </Router>
   );
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,13 +2,28 @@
 import { useWeather } from "../hooks/useWeather";
 
 const Home = () => {
-  const {  loading, error } = useWeather("Simpsonville");
+  const { weather, loading, error } = useWeather("Simpsonville");
 
   return (
     <div className="p-8 flex justify-center">
       {loading && <p>Loading weather...</p>}
       {error && <p className="text-red-500">{error}</p>}
-      TESTING WEATHERAPP DEPLOYMENT WORKING?
+      {weather && (
+        <div className="text-center">
+          <h2 className="text-2xl font-bold">
+            {weather.location.name}, {weather.location.country}
+          </h2>
+          <p className="text-lg">{weather.current.condition.text}</p>
+          <img
+            src={`https:${weather.current.condition.icon}`}
+            alt="Weather Icon"
+            className="mx-auto"
+          />
+          <p className="text-xl">
+            {weather.current.temp_c}°C / {weather.current.temp_f}°F
+          </p>
+        </div>
+      )}
     </div>
   );
 
