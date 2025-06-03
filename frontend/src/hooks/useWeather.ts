49ad44022ad038298e40deb7b60ab4f04788607e
Keenan Gunter <keenangunter123@gmail.com>
2025-06-02 21:32:29 -0400

    Working on fixing actions and aws deployment

--- frontend/src/hooks/useWeather.ts
+++ frontend/src/hooks/useWeather.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from "react";
 import type { GetCurrentWeatherResponse } from "../types/currentweather";
-const baseUrl = import.meta.env.VITE_API_URL;
 
 export const useWeather = (city: string) => {
   const [weather, setWeather] = useState<GetCurrentWeatherResponse | null>(null);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
+  const baseUrl = import.meta.env.VITE_API_URL || '/api';
 
   useEffect(() => {
-    fetch(`${baseUrl}/api/weather?city=${encodeURIComponent(city)}`)
+    fetch(`${baseUrl}/weather?city=${encodeURIComponent(city)}`)
       .then((res) => {
         if (!res.ok) throw new Error(`HTTP ${res.status}`);
         return res.json();
