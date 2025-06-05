c9b1b167181d0cbb76a233587d77e299a4c1e377
Keenan Gunter <keenangunter123@gmail.com>
2025-06-05 18:02:41 -0400

    Header fix for clarification

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -113,7 +113,7 @@ const Home = ({ city }: HomeProps) => {
 
             <div className="my-8 border-t border-blue-800" />
             <h3 className="text-2xl font-semibold text-gray-900 mb-6 text-center">
-              Next 8 Days Forecast
+              8-Day Forecast
             </h3>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
               {forecast.forecast.forecastday
